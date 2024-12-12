2f943cddf65927167014bdaff9533687bff84479
ioneacre <ian.oneacre@cloudimperiumgames.com>
2024-12-12 13:04:43 -0700

    Added schema validation testing script and update flask app to use.

--- flask_json_processor.py
+++ flask_json_processor.py
@@ -21,72 +21,84 @@ logging.basicConfig(
 
 logger = logging.getLogger()
 
-# TODO simplify schema to avoid nesting maybe. maybe process nested to be thorough
-# schema = {
-#     "type": "object",
-#     "properties": {
-#         "transaction_id": {"type": "string", "minLength": 36},
-#         "customer_id": {"type": "integer", "minLength": 5, "maxLength": 5},
-#         "customer_name": {"type": "string"},
-#         "transaction_date": {"type": "timestamp"},
-#         "items": {"type": "array", "items": {"type": "string"}},
-#         "cash_payment_pennies": {"type": "integer", "min": 0, "max": 10000},
-#         "credit_payment_pennies": {"type": "integer", "min": 0, "max": 10000},
-#         "shipping_address": {"type": "array", "items": {"type": "string"}},
-#         "region": {"type": "string"},
-#     },
-#     "required": ["transaction_id", "customer_id", "customer_name", "transaction_date", "items",
-#                  "cash_payment_pennies", "credit_payment_pennies"],
-#     "additionalProperties": False
-# }
+schema = {
+    "type": "object",
+    "properties": {
+        "transaction_id": {"type": "string", "minLength": 36},
+        "customer_id": {"type": "integer", "minLength": 5, "maxLength": 5},
+        "customer_name": {"type": "string"},
+        "transaction_date": {"type": "string", "format": "date-time"},
+        "items": {"type": "array"},
+        "cash_payment_pennies": {"type": "integer", "min": 0, "max": 10000},
+        "credit_payment_pennies": {"type": "integer", "min": 0, "max": 10000},
+        "shipping_address": {
+            "type": "object",
+            "properties": {
+                "street": {"type": "string"},
+                "city": {"type": "string"},
+                "state": {"type": "string"},
+                "zip_code": {"type": "integer"}
+            }
+        },
+        "region": {"type": "string"},
+    },
+    "required": ["transaction_id", "customer_id", "customer_name", "transaction_date", "items",
+                 "cash_payment_pennies", "credit_payment_pennies"],
+    "additionalProperties": False
+}
 
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+#
+# db = SQLAlchemy(app)
+#
+#
+# class Order(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     uuid = db.Column(db.String(36), nullable=False)
+#     account_id = db.Column(db.String(50), nullable=False)
+#     order_id = db.Column(db.String(50), nullable=False)
+#     order_date = db.Column(db.DateTime, default=datetime.utcnow)
+#     product_id = db.Column(db.String(50), nullable=False)
+#     product_description = db.Column(db.String(255), nullable=False)
+#     unit_price = db.Column(db.Float, nullable=False)
+#     quantity = db.Column(db.Integer, nullable=False)
+#     order_total = db.Column(db.Float, nullable=False)
+#     cash_total = db.Column(db.Float, nullable=False)
+#     credit_total = db.Column(db.Float, nullable=False)
+#     tax_applicable = db.Column(db.Boolean, nullable=False)
+#     billing_zip_code = db.Column(db.String(10), nullable=False)
+#     ip_address = db.Column(db.String(45), nullable=False)
+#
+#
+# # Initialize the database
+# with app.app_context():
+#     db.create_all()
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-db = SQLAlchemy(app)
-
-
-class Order(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    uuid = db.Column(db.String(36), nullable=False)
-    account_id = db.Column(db.String(50), nullable=False)
-    order_id = db.Column(db.String(50), nullable=False)
-    order_date = db.Column(db.DateTime, default=datetime.utcnow)
-    product_id = db.Column(db.String(50), nullable=False)
-    product_description = db.Column(db.String(255), nullable=False)
-    unit_price = db.Column(db.Float, nullable=False)
-    quantity = db.Column(db.Integer, nullable=False)
-    order_total = db.Column(db.Float, nullable=False)
-    cash_total = db.Column(db.Float, nullable=False)
-    credit_total = db.Column(db.Float, nullable=False)
-    tax_applicable = db.Column(db.Boolean, nullable=False)
-    billing_zip_code = db.Column(db.String(10), nullable=False)
-    ip_address = db.Column(db.String(45), nullable=False)
-
-
-# Initialize the database
-with app.app_context():
-    db.create_all()
-
+'''
+Main processing for incoming events.
+In production, any event caught in exceptions would be written to persistent storage for review 
+and potential reprocessing.
+'''
 
 @app.route('/ingest', methods=['POST'])
 def process_data():
     try:
-        data = request.get_json(force=True)
+        # Ensure payload is formatted properly and validate schema
+        payload = request.get_json(force=True)
 
-        # try:
-        #     validate(instance=data, schema=schema)
-        #
-        # except ValidationError as e:
-        #     # Log the malformed schema
-        #     logger.error(f"Incorrect schema: {data}")
-        #
-        #     return jsonify({"error": str(e)}), 400
+        # Intentionally cast customer_id as string in producer
+        field_key = "customer_id"
 
-        return jsonify({"status": "success", "message": "Event processed successfully"}), 200, data
+        payload[field_key] = int(payload[field_key])
 
-    except BadRequest as e:
+        validate(instance=payload, schema=schema)
+
+        print("Event Processed Successfully")
+
+        return jsonify({"status": "success", "message": "Event processed successfully"}), 200, payload
+
+    except BadRequest:
         # Log the malformed JSON request
         logger.error(f"Malformed JSON: {request.data.decode('utf-8')}")
 
@@ -94,10 +106,22 @@ def process_data():
 
     except ValueError as e:
         # Handle custom validation errors
+        logger.error(f"ValueError: {e}")
+
         return jsonify({"status": "error", "message": str(e)}), 422
+
+    except ValidationError as e:
+        # Handle JSON schema validation errors
+        message = e.schema["error_msg"] if "error_msg" in e.schema else e.message
+
+        logger.error(f"Schema validation failed: {message}")
+
+        return jsonify({"status": "error", "message": message})
 
     except Exception as e:
         # Catch-all for unexpected errors
+        logger.error(e)
+
         return jsonify({"status": "error", "message": "An unexpected error occurred"}), 500
 
 
